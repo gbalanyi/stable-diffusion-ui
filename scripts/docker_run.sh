fcfc3b991dd70708ca11ea485a5900f1f2a8cb05
Gabor Balanyi <43302710+gbalanyi@users.noreply.github.com>
2023-04-01 12:55:36 +0200

    Update docker_run.sh

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -11,9 +11,8 @@ echo "PYTHONPATH=$PYTHONPATH"
 which python
 python --version
 
-cd ../..
 export SD_UI_PATH=`pwd`/ui
-cd stable-diffusion
+cd /easy-diffusion/stable-diffusion
 
 uvicorn main:server_api --app-dir "$SD_UI_PATH" --port ${SD_UI_BIND_PORT:-9000} --host ${SD_UI_BIND_IP:-0.0.0.0} --log-level error
 
