061ee9c374036a9442dde02b693ee7f5cd6794d8
Gabor Balanyi <43302710+gbalanyi@users.noreply.github.com>
2023-04-01 09:34:18 +0200

    Created docker_run.sh

---
+++ scripts/docker_run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+printf "\n\nStarting the server!\n\n"
+
+SD_PATH=`pwd`
+
+export PYTORCH_ENABLE_MPS_FALLBACK=1
+export PYTHONPATH="$INSTALL_ENV_DIR/lib/python3.8/site-packages"
+echo "PYTHONPATH=$PYTHONPATH"
+
+which python
+python --version
+
+cd ..
+export SD_UI_PATH=`pwd`/ui
+cd stable-diffusion
+
+uvicorn main:server_api --app-dir "$SD_UI_PATH" --port ${SD_UI_BIND_PORT:-9000} --host ${SD_UI_BIND_IP:-0.0.0.0} --log-level error
+
+read -p "Press any key to continue"
