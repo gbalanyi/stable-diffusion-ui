a54efcac274899236f35c6c452be43c2ff29c2b6
Gabor Balanyi <balanyi.g@gmail.com>
2024-12-24 13:35:50 +0100

    Bump version to 3.0

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM nvidia/cuda:12.2.2-runtime-ubuntu22.04
+#FROM nvidia/cuda:11.4.3-cudnn8-runtime-ubuntu20.04
+#FROM nvidia/cuda:11.6.2-cudnn8-runtime-ubuntu20.04
+#FROM nvidia/cuda:12.1.0-runtime-ubuntu22.04
+#FROM debian:stable-slim
+
+ENV DEBIAN_FRONTEND=noninteractive
+ENV TZ=Europe/Budapest
+
+RUN apt-get update && apt-get install -y curl git bzip2 libgl1-mesa-glx libglib2.0-0 && rm -rf /var/lib/apt/lists/*
+
+COPY easy-diffusion /easy-diffusion
+
+WORKDIR /easy-diffusion
+
+RUN chmod +x start.sh && /bin/bash start.sh
+RUN chmod +x scripts/docker_run.sh
+
+EXPOSE 9000
+
+ENTRYPOINT ["scripts/docker_run.sh"]
--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -2,20 +2,12 @@
 
 printf "\n\nStarting the server!\n\n"
 
-SD_PATH=`pwd`
-
 export INSTALL_ENV_DIR="$(pwd)/installer_files/env"
 export PATH="$(pwd)/installer_files/env/bin:$PATH"
-export PYTORCH_ENABLE_MPS_FALLBACK=1
-export PYTHONPATH="$INSTALL_ENV_DIR/lib/python3.8/site-packages"
-echo "PYTHONPATH=$PYTHONPATH"
 
 which python
 python --version
 
-export SD_UI_PATH=`pwd`/ui
-cd /easy-diffusion/stable-diffusion
-
-uvicorn main:server_api --app-dir "$SD_UI_PATH" --port ${SD_UI_BIND_PORT:-9000} --host ${SD_UI_BIND_IP:-0.0.0.0} --log-level error
+python scripts/check_modules.py --launch-uvicorn
 
 read -p "Press any key to continue"
